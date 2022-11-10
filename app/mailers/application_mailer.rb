66598c76e7380e5e3be7e227c7b31ee619c5fec4
Leonard Ng'eno <dranoel.bravo@gmail.com>
2022-11-10 17:40:43 -0500

    change default from email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: Rails.application.credentials.config[:from_email]
+  default from: Rails.application.credentials.config[:default_from_email]
   layout 'mailer'
 end
--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,6 +1,6 @@
 Clearance.configure do |config|
-  config.mailer_sender = Rails.application.credentials.config[:from_email]
+  config.mailer_sender = Rails.application.credentials.config[:default_from_email]
   config.rotate_csrf_on_sign_in = true
-  config.cookie_expiration = lambda { |cookies| 1.hour.from_now.utc }
+  config.cookie_expiration = ->(_cookies) { 1.hour.from_now.utc }
   config.allow_sign_up = false
 end
