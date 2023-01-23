d2079e75711e54d93e7a8577b7d59cc935c61437
Leonard Ng'eno <dranoel.bravo@gmail.com>
2023-01-23 16:02:02 -0500

    test branch

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -11,8 +11,8 @@ import "bootstrap/dist/css/bootstrap.css"
 
 let prefix = "/";
 if (process.env.NODE_ENV !== 'development') {
-  // prefix = "/test_smile/";
-  prefix = "/real_smile/";
+  prefix = "/test_smile/";
+  // prefix = "/real_smile/";
 }
 Vue.prototype.$basePrefix = prefix;
 
