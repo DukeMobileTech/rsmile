ea870d4b7668a3cb4c9ca4a47dbb28a45422ef35
Leonard Ng'eno <dranoel.bravo@gmail.com>
2023-08-30 12:14:09 -0400

    add source totals to agency table

--- app/models/survey_responses/agency.rb
+++ app/models/survey_responses/agency.rb
@@ -5,21 +5,33 @@ module SurveyResponses
       baselines = SurveyResponse.eligible_completed_main_block.where(country: country)
       SurveyResponse::ELIGIBLE_SGM_GROUPS.each do |group|
         group_baselines = baselines.select { |baseline| baseline.sgm_group == group }
-        row = {
-          'SGM Group': group,
-          Recruited: group_baselines.size
-        }
-        source_stats = {}
-        group_baselines.each do |baseline|
-          baseline.source&.split(',')&.each do |source|
-            list = source_stats[source] ||= []
-            source_stats[source] = list << baseline
-          end
-        end
-        (12..24).each do |source|
-          row[SurveyResponse::SOURCES[source.to_s]] = source_stats[source.to_s]&.size || 0
-        end
+        row = row_data(group, group_baselines)
         stats << row
+      end
+      stats
+    end
+
+    private
+
+    def row_data(group, group_baselines)
+      row = {
+        group: group,
+        recruited: group_baselines.size
+      }
+      source_stats = baseline_source_stats(group_baselines)
+      (12..24).each do |source|
+        row[source.to_s] = source_stats[source.to_s]&.size || 0
+      end
+      row
+    end
+
+    def baseline_source_stats(group_baselines)
+      stats = {}
+      group_baselines.each do |baseline|
+        baseline.source&.split(',')&.each do |source|
+          list = stats[source] ||= []
+          stats[source] = list << baseline
+        end
       end
       stats
     end
