396125e861371ff03c9ce7c62119cf772b401d5d
Leonard Ng'eno <dranoel.bravo@gmail.com>
2023-09-01 11:36:09 -0400

    add target to home page

--- app/javascript/packs/components/Timeline.vue
+++ app/javascript/packs/components/Timeline.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card mb-5">
     <div class="card-header">
-      <h6 class="card-title">Timeline</h6>
+      <h6 class="card-title">Recruitment Timeline</h6>
     </div>
     <div class="card-body">
       <LineChart v-if="loaded" :chartdata="chartData" :options="chartOptions" />
@@ -16,128 +16,161 @@
 import axios from 'axios';
 import LineChart from './charts/LineChart';
 
-  export default {
-    name: 'Timeline',
+export default {
+  name: 'Timeline',
 
-    data: () => ({
-      loaded: false,
-      chartOptions: {
-        scales: {
-          yAxes: [{
+  data: () => ({
+    loaded: false,
+    chartOptions: {
+      scales: {
+        yAxes: [
+          {
             ticks: {
-              beginAtZero: true
+              beginAtZero: true,
             },
             gridLines: {
-              display: true
-            }
-          }],
-          xAxes: [ {
+              display: true,
+            },
+          },
+        ],
+        xAxes: [
+          {
             gridLines: {
-              display: false
-            }
-          }]
-        },
-        legend: {
-          display: true
-        },
-        responsive: true,
-        maintainAspectRatio: false
+              display: false,
+            },
+          },
+        ],
       },
-      chartData: {},
-      colors: {
-        'Kenya': '#006600',
-        'Kenya (cumulative)': '#000000',
-        'Vietnam': '#DA251D',
-        'Vietnam (cumulative)': '#FFCD00',
-        'Brazil': '#0C87D1',
-        'Brazil (cumulative)': '#002776',
-        'All': '#732982',
-      }
-    }),
-
-    components: {
-      LineChart,
+      legend: {
+        display: true,
+      },
+      responsive: true,
+      maintainAspectRatio: false,
     },
-
-    mounted: function () {
-      this.fetchData();
+    chartData: {},
+    colors: {
+      Kenya: '#006600',
+      'Kenya (cumulative)': '#000000',
+      Vietnam: '#DA251D',
+      'Vietnam (cumulative)': '#FFCD00',
+      Brazil: '#0C87D1',
+      'Brazil (cumulative)': '#002776',
+      All: '#732982',
+      Target: '#FF7F00',
     },
+  }),
 
-    methods: {
-      fetchData() {
-        this.loaded = false;
-        axios.get(`${this.$basePrefix}participants/weekly_participants`)
-        .then(response => {
-          if (typeof response.data == "string" && response.data.startsWith("<!DOCTYPE html>")) {
+  components: {
+    LineChart,
+  },
+
+  mounted: function () {
+    this.fetchData();
+  },
+
+  methods: {
+    fetchData() {
+      this.loaded = false;
+      axios
+        .get(`${this.$basePrefix}participants/weekly_participants`)
+        .then((response) => {
+          if (
+            typeof response.data == 'string' &&
+            response.data.startsWith('<!DOCTYPE html>')
+          ) {
             window.location.reload();
           }
-          let datasets = [];
           let countries = Object.keys(response.data);
-          let dates = [];
-          Object.values(response.data)[0].forEach((weeklyStats) => {
-            dates.push(Object.keys(weeklyStats)[0]);
-          });
           let allWeeklyCounts = {};
-          countries.forEach((country) => {
-            let weeklyCounts = [];
-            let cumulativeCounts = [];
-            let total = 0;
-            response.data[country].forEach((countryData) => {
-              let count = Object.values(countryData)[0];
-              total += count;
-              weeklyCounts.push(count);
-              cumulativeCounts.push(total);
-              let date = Object.keys(countryData)[0];
-              if (allWeeklyCounts[date]) {
-                allWeeklyCounts[date] += count;
-              } else {
-                allWeeklyCounts[date] = count;
-              }
-            });
-            let color = this.colors[country];
-            datasets.push({
-              label: country,
-              fill: false,
-              borderWidth: 1,
-              borderColor: color,
-              backgroundColor: color,
-              data: weeklyCounts,
-            });
-            let cumulativeColor = this.colors[country + " (cumulative)"];
-            datasets.push({
-              label: country + " (cumulative)",
-              fill: false,
-              borderWidth: 1,
-              borderColor: cumulativeColor,
-              backgroundColor: cumulativeColor,
-              data: cumulativeCounts,
-            });
-          });
-          let allCumulativeCounts = [];
-          let cumulativeTotal = 0;
-          Object.values(response.data)[0].forEach((weeklyStats) => {
-            let week = Object.keys(weeklyStats)[0];
-            cumulativeTotal += allWeeklyCounts[week];
-            allCumulativeCounts.push(cumulativeTotal);
-          });
+          let datasets = this.getDatasets(countries, response, allWeeklyCounts);
+          let dates = this.getDates(response);
+          let allCumulativeCounts = this.getCumulativeTotals(
+            response,
+            allWeeklyCounts
+          );
           let allColor = this.colors['All'];
-            datasets.push({
-              label: 'SMILE (cumulative)',
-              fill: false,
-              borderWidth: 1,
-              borderColor: allColor,
-              backgroundColor: allColor,
-              data: allCumulativeCounts,
-            });
+          let targetColor = this.colors['Target'];
+          datasets.push({
+            label: 'SMILE (cumulative)',
+            fill: false,
+            borderWidth: 1,
+            borderColor: allColor,
+            backgroundColor: allColor,
+            data: allCumulativeCounts,
+          });
+          datasets.push({
+            label: 'Study Target',
+            fill: false,
+            borderWidth: 1,
+            borderColor: targetColor,
+            backgroundColor: targetColor,
+            data: allCumulativeCounts.map((count) => 10000),
+          });
           this.chartData = {
             labels: dates,
             datasets: datasets,
           };
           this.loaded = true;
         });
-      },
     },
-  }
+    getDates(response) {
+      let dates = [];
+      Object.values(response.data)[0].forEach((weeklyStats) => {
+        dates.push(Object.keys(weeklyStats)[0]);
+      });
+      return dates;
+    },
+    getDatasets(countries, response, allWeeklyCounts) {
+      let datasets = [];
+      countries.forEach((country) => {
+        let weeklyCounts = [];
+        let cumulativeCounts = [];
+        let total = 0;
+        response.data[country].forEach((countryData) => {
+          let count = Object.values(countryData)[0];
+          total += count;
+          weeklyCounts.push(count);
+          cumulativeCounts.push(total);
+          let date = Object.keys(countryData)[0];
+          if (allWeeklyCounts[date]) {
+            allWeeklyCounts[date] += count;
+          } else {
+            allWeeklyCounts[date] = count;
+          }
+        });
+        let color = this.colors[country];
+        datasets.push({
+          label: country,
+          fill: false,
+          borderWidth: 1,
+          borderColor: color,
+          backgroundColor: color,
+          data: weeklyCounts,
+        });
+        let cumulativeColor = this.colors[country + ' (cumulative)'];
+        datasets.push({
+          label: country + ' (cumulative)',
+          fill: false,
+          borderWidth: 1,
+          borderColor: cumulativeColor,
+          backgroundColor: cumulativeColor,
+          data: cumulativeCounts,
+        });
+      });
+      return datasets;
+    },
+    getCumulativeTotals(response, allWeeklyCounts) {
+      let allCumulativeCounts = [];
+      let cumulativeTotal = 0;
+      Object.values(response.data)[0].forEach((weeklyStats) => {
+        let week = Object.keys(weeklyStats)[0];
+        cumulativeTotal += allWeeklyCounts[week];
+        allCumulativeCounts.push(cumulativeTotal);
+      });
+      return allCumulativeCounts;
+    },
+  },
+};
 </script>
 
 <style scoped>
