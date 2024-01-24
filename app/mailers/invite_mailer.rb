0b86e294ce636355d8ddfc0fc4d6bb030cd69b0e
Leonard Ng'eno <dranoel.bravo@gmail.com>
2024-01-24 11:12:25 -0500

    change email title

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -1,8 +1,6 @@
 class InviteMailer < ApplicationMailer
-
   def invite_email
     @invite = params[:invite]
-    mail(to: @invite.email, :subject => "Welcome to SMILE Dashboard")
+    mail(to: @invite.email, subject: 'Welcome to SMILE RDS Dashboard')
   end
-
 end
