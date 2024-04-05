6b88027006ced47c5723b7092db57f86129907f6
Leonard Ng'eno <dranoel.bravo@gmail.com>
2024-04-05 14:14:43 -0400

    add sw translations

--- app/admin/email_logs.rb
+++ app/admin/email_logs.rb
@@ -3,6 +3,13 @@ ActiveAdmin.register EmailLog do
     column :id
     column :recipient
     column :subject
-    column :message do |log| raw(log.message) end
+    column :message do |log|
+      # rubocop:disable Rails/OutputSafety
+      raw(log.message)
+      # rubocop:enable Rails/OutputSafety
+    end
+    column :created_at
+    column :updated_at
+    actions
   end
 end
