926add12ac6064446a5aec8b4ee8920f861cc444
Leonard Ng'eno <dranoel.bravo@gmail.com>
2024-07-16 15:49:35 -0400

    email seeds consent file

--- app/controllers/api/v1/consents_controller.rb
+++ app/controllers/api/v1/consents_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::ConsentsController < Api::ApiController
   def create
-    ConsentMailer.with(email: params[:email], language: params[:language]&.downcase,
-                       country: params[:country][0, 2]&.downcase).consent_form.deliver_now
+    ConsentMailer.with(email: params[:email],
+                       language: params[:language]&.downcase,
+                       country: params[:country][0, 2]&.downcase,
+                       seed: params[:seed]).consent_form.deliver_now
     render json: { email: 'sent' }, status: :ok
   rescue Exception
     render json: { email: 'failed' }, status: :unprocessable_entity
--- app/mailers/consent_mailer.rb
+++ app/mailers/consent_mailer.rb
@@ -3,6 +3,7 @@ class ConsentMailer < ApplicationMailer
     @language = params[:language]
     filename = @language
     filename = "#{@language}-#{params[:country]}" if @language == 'en' && %w[ke vi].include?(params[:country])
+    filename = "#{filename}-seeds" if params[:seed]
     attachments['consent.pdf'] = File.read("#{Rails.root}/storage/consent/#{filename}.pdf")
     mail(to: params[:email], subject: I18n.t('form', locale: @language))
   end
